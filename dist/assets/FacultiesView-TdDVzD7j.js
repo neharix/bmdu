50d39a11a1932e328f989f52bde35a168893e5dc
neharix <142607660+neharix@users.noreply.github.com>
2025-01-29 02:02:26 +0500

    upd

---
+++ dist/assets/FacultiesView-TdDVzD7j.js
@@ -0,0 +1,1 @@
+import{_ as e,q as c,m as t}from"./index-C3pZ3GpE.js";const n={};function r(s,a){return t(),c("p",null,"faculties")}const _=e(n,[["render",r]]);export{_ as default};
---
+++ dist/assets/SecondarySchoolsView-BJuKq52H.js
@@ -0,0 +1,1 @@
+import{_ as e,q as c,m as o}from"./index-C3pZ3GpE.js";const n={};function r(s,t){return o(),c("p",null,"secondary-schools")}const _=e(n,[["render",r]]);export{_ as default};
