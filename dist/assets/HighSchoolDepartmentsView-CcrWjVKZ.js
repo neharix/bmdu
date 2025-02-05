05406ba969492ba1e8c42bfb434ec64df4b88761
neharix <142607660+neharix@users.noreply.github.com>
2025-02-05 21:29:15 +0500

    update

---
+++ dist/assets/HighSchoolDepartmentsView-CcrWjVKZ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-Dbb6q-gd.js";const t={};function r(c,n){return" departments "}const s=e(t,[["render",r]]);export{s as default};
---
+++ dist/assets/HighSchoolSpecializationsView-BhQfREPu.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./index-Dbb6q-gd.js";const e={};function i(o,t){return" specialization "}const r=c(e,[["render",i]]);export{r as default};
