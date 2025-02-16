99c53eab810ba52df53397044bbbd45e5536c0c6
neharix <142607660+neharix@users.noreply.github.com>
2025-02-16 13:04:59 +0500

    update

---
+++ dist/assets/Page403-DqCBSOte.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./TheError-B_0Jp2wa.js";import{c as a,b as s}from"./index-C_dqcgSE.js";const m={__name:"Page403",setup(t){return(o,r)=>(s(),a(e,{"status-code":403,message:"rugsat ýok","is-home-link-enabled":!0}))}};export{m as default};
