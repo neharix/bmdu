404ffe45987bb1c9aa370ecf5dbad70908fa8e0a
neharix <142607660+neharix@users.noreply.github.com>
2025-02-21 17:15:12 +0500

    update

---
+++ dist/assets/Page403-BTqm7MoC.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./TheError-Omuk72Io.js";import{c as a,b as s}from"./index-CsGQU5AY.js";const m={__name:"Page403",setup(t){return(o,r)=>(s(),a(e,{"status-code":403,message:"rugsat ýok","is-home-link-enabled":!0}))}};export{m as default};
