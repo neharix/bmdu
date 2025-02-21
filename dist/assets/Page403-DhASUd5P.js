e77dad4aae15439a9ed63b1e4aa87f7d81d105f4
neharix <142607660+neharix@users.noreply.github.com>
2025-02-21 17:12:14 +0500

    Delete dist directory

--- dist/assets/Page403-DhASUd5P.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./TheError-uAhbU1QO.js";import{c as a,b as s}from"./index-BsvZMky9.js";const m={__name:"Page403",setup(t){return(o,r)=>(s(),a(e,{"status-code":403,message:"rugsat ýok","is-home-link-enabled":!0}))}};export{m as default};
